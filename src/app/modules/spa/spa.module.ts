61e037e69f8a1a77a0812a8d538bdc9b99f38359
KarenCortes <karenj.cortesn@gmail.com>
2024-02-10 12:23:36 -0500

    avance sprint-review1 componentes

---
+++ src/app/modules/spa/spa.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpaRoutingModule } from './spa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SpaRoutingModule
+  ]
+})
+export class SpaModule { }
