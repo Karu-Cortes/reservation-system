cee2a4572eb6e7818768eb2695d3aabe60d99fdd
KarenCortes <karenj.cortesn@gmail.com>
2024-02-13 16:22:41 -0500

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ReservationComponent } from './shared/components/reservation/reservation.component';
+import { ReservationListComponent } from './shared/components/reservation-list/reservation-list.component';
+import { LogInComponent } from './shared/components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReservationComponent,
+    ReservationListComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
